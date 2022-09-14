b659332a4a7c4f628fc76ead1df81e1c06c1f8b4
AndrewHarig <97308299+AndrewHarig@users.noreply.github.com>
2022-09-14 14:13:41 -0400

    Update and rename Lab-1.18 to Lab-1.18.py

---
+++ Chapter 1/Lab-1.18.py
@@ -0,0 +1,14 @@
+#takes the first line as an input and sets it as a string named first_name
+first_name = input()
+
+#takes the second line as an input and sets it as a string named generic_location
+generic_location = input()
+
+#takes the third line as an input and sets it as and integer named whole_number
+whole_number = int(input())
+
+#takes the fourth line as an input and sets it as and string named plural_noun
+plural_noun = input()
+
+#print statement using the variablesto create a sentence
+print(first_name, 'went to', generic_location, 'to buy', whole_number, 'different types of', plural_noun)
